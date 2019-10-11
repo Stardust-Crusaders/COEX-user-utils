b6c175a23dac5b914f6e8cf4f5287d9ae0fef9d1
Skiperlx <6522245@gmail.com>
2019-10-11 17:29:39 +0300

    optimized code, adding HUD

---
+++ scripts/HudController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Net.Mime;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HudController : MonoBehaviour
+{
+    private string _dataMes;
+    private System.DateTime _curTime;
+    public GameObject data;
+    private UnityEngine.UI.Text _compText;
+
+    void Start()
+    {
+        _compText = data.GetComponent<Text>();
+    }
+    void Update()
+    {
+        _curTime = System.DateTime.Now;
+
+        _dataMes = _curTime.Hour + ":" + _curTime.Minute;
+
+        _compText.text = _dataMes;
+    }
+
+
+}
